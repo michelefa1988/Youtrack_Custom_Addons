56d4c189755c4c482d7c9d1ce4ee8c90a7b323a5
Zetta-Michele <m@fenechadami.eu>
2016-08-22 11:39:50 +0200

    added help parameter

--- index.js
+++ index.js
@@ -2,23 +2,38 @@ var request = require('request');
 
 var login = require('./Login.js');
 var issues = require('./GetIssues.js');
-var ExportToPDF = require ('./ExportToPDF');
+var ExportToPDF = require('./ExportToPDF');
 
-login.logon(function(err, setcookie) {
-    if (!err) {
-        var options = {
-            url: 'https://zettabox.myjetbrains.com/youtrack/rest/admin/project',
-            headers: {
-                'Cookie': setcookie.join(" ; "),
-            }
-        };
-        var ticketsArr = [];
-        issues.getIssues(function(error,ticketsArr) {
-            console.log("********CALLBACK*********************");
+function printHelp() {
+    console.log("------------------------------------------------------------------------------------------");
+    console.log("Youtrack helper tool");
+    console.log("");
+    console.log("-email --> Enter valid youtrack email address");
+    console.log("-pass --> Enter a valid youtrack password");
+    console.log("------------------------------------------------------------------------------------------");
+}
 
-            //console.log(ticketsArr);
-            ExportToPDF.writetoPDF();
+//if help is flag is present display help
+if (process.argv.indexOf("-help") != -1) { //does our flag exist?
+    printHelp();
+} else { //read arguments and continue processing
 
-        });
-    }
-});
+    login.logon(function(err, setcookie) {
+        if (!err) {
+            var options = {
+                url: 'https://zettabox.myjetbrains.com/youtrack/rest/admin/project',
+                headers: {
+                    'Cookie': setcookie.join(" ; "),
+                }
+            };
+            var ticketsArr = [];
+            issues.getIssues(function(error, ticketsArr) {
+                console.log("********CALLBACK*********************");
+
+                //console.log(ticketsArr);
+                ExportToPDF.writetoPDF();
+
+            });
+        }
+    });
+}
