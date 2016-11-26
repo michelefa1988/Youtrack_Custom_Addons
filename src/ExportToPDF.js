2c76911905ea4920b04a99539349418955461dd7
Michele Fenech Adami <michele@zettabox.com>
2016-11-26 15:40:35 +0100

    Parameterized PDF creation

--- src/ExportToPDF.js
+++ src/ExportToPDF.js
@@ -1,3 +1,4 @@
+var conf = require('./config.js');
 PDFDocument = require('pdfkit');
 fs = require('fs');
 
@@ -47,7 +48,7 @@ function createPDFDocument() {
 
     // Pipe its output somewhere, like to a file or HTTP response
     // See below for browser usage
-    var stream = fs.createWriteStream('out-pdf.pdf');
+    var stream = fs.createWriteStream(conf.PDF_Name);
     doc.pipe(stream);
 
     stream.on('error', function(err) {
@@ -113,7 +114,7 @@ module.exports = {
                     }
 
                     //display Ticket number
-                    doc.font('Times_New_Roman_Bold.ttf')
+                    doc.font('./assets/fonts/Times_New_Roman_Bold.ttf')
                         .fontSize(50)
                         .fillColor('black')
                         .text(page[ticketNo][0].replace(re, function(matched) {
@@ -123,11 +124,11 @@ module.exports = {
 
                     //display Header of ticket
                     doc.fontSize(13);
-                    doc.font('Times_New_Roman_Normal.ttf');
+                    doc.font('./assets/fonts/Times_New_Roman_Normal.ttf');
                     doc.text(polishString(page[ticketNo][6]));
 
                     //display story points
-                    doc.font('Times_New_Roman_Normal.ttf')
+                    doc.font('./assets/fonts/Times_New_Roman_Normal.ttf')
                         .fontSize(30)
                         .fillColor('black')
                         .text(page[ticketNo][1], x + 200, (y * 260) + 10);
@@ -135,13 +136,13 @@ module.exports = {
 
                     //display Asignee first name
                     var FullName = page[ticketNo][2].split(" ");
-                    doc.font('Times_New_Roman_Normal.ttf')
+                    doc.font('./assets/fonts/Times_New_Roman_Normal.ttf')
                         .fontSize(20)
                         .fillColor('black')
                         .text(FullName[0], x + 10, (y * 260) + 10);
 
                     //write QR image
-                    createQR('https://zettabox.myjetbrains.com/youtrack/issue/'+ page[ticketNo][0], page[ticketNo][0]);
+                    createQR(conf.YouTrack_Search_URL + page[ticketNo][0], page[ticketNo][0]);
                     doc.image('temp/' + page[ticketNo][0] + '.PNG', x + 100, (y * 260) + 198 , {width:50});
 
                     if (AddedToSprint) {
